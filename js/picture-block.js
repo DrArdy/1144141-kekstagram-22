49d5f8b895e7bf294de3008797a2c22522fcca96
DrArdy <dimakharitonov2011@yandex.ru>
2021-03-10 11:23:16 +0300

    Правки

--- js/picture-block.js
+++ js/picture-block.js
@@ -9,7 +9,7 @@ photosSpecifications.forEach(({url, likes, comments}) => {
   const picturePopup = picturePopupTemplate.cloneNode(true);
   picturePopup.querySelector('.picture__img').src = url;
   picturePopup.querySelector('.picture__likes').textContent = likes;
-  picturePopup.querySelector('.picture__comments').textContent = comments;
+  picturePopup.querySelector('.picture__comments').textContent = comments.length;
   similarPhotosFragment.appendChild(picturePopup);
 });
 photosPool.appendChild(similarPhotosFragment);
