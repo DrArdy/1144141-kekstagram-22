ea088b1ad22f9bddd2a559758409ff40551c1371
DrArdy <dimakharitonov2011@yandex.ru>
2021-03-28 09:57:23 +0300

    Начало Работы

--- js/picture-block.js
+++ js/picture-block.js
@@ -5,6 +5,10 @@ const photosPool = document.querySelector('.pictures');
 const picturePopupTemplate = document.querySelector('#picture')
   .content 
   .querySelector('.picture');
+const filtersBlock = document.querySelector('.img-filters');
+const filterDiscussedButton = filtersBlock.querySelector('#filter-discussed');
+const filterRandomButton = filtersBlock.querySelector('#filter-random');
+const filterDefaultButton = filtersBlock.querySelector('#filter-default');
 
 const renderPicture = (pictureData) => {
   const picturePopup = picturePopupTemplate.cloneNode(true);
@@ -23,14 +27,60 @@ const renderPictures = (dataList) => {
   dataList.forEach((array) => {
     photosListFragment.appendChild(renderPicture(array));
   });
-
+  
   photosPool.appendChild(photosListFragment);
 };
 
 const renderPicturesFromServer = () => {
   getServerData((dataList) => {
     renderPictures(dataList);
+    initFilters(dataList);
   });
 };
+
+const initFilters = (dataList) => {
+  filtersBlock.classList.remove('img-filters--inactive');
+  
+  filterDiscussedButton.addEventListener('click', renderByDiscussed(dataList));
+  filterRandomButton.addEventListener('click', renderByRandom(dataList));
+  filterDefaultButton.addEventListener('click', renderByDefault(dataList));
+};
+
+const renderByDiscussed = (dataList)  => () => {
+  clearPictures();
+  renderPictures(sortByDiscussed(dataList));
+};
+
+const renderByDefault = (dataList) => () => {
+  clearPictures();
+  renderPictures(dataList);
+};
+
+const renderByRandom = (dataList) => () => {
+  clearPictures();
+  renderPictures(shuffleArray(dataList));
+};
+
+const clearPictures = () => {
+  while (photosPool.firstChild) {
+    photosPool.removeChild(photosPool.firstChild);
+  }
+};
+
+const shuffleArray = (dataList) => {
+  for(let i = dataList.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random()*(i + 1));
+    let temp = dataList[j];
+    dataList[j] = dataList[i];
+    dataList[i] = temp;
+  }
+  return dataList;
+};
+
+const sortByDiscussed = (dataList) => {
+  dataList.sort((a, b) => {
+    return b.comments.length - a.comments.length;
+  });
+}
 
 export {renderPicturesFromServer};
